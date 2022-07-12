537604f11b333b279087d1f3f087503f5404c041
gralmeidan <gralmeidan@gmail.com>
2022-07-12 15:33:10 -0400

    Added empty config files

---
+++ src/config/disc.cjs
@@ -0,0 +1,9 @@
+const DISC_TOKEN = '';
+const DISC_GUILD_ID = '';
+const DISC_CLIENT_ID = '';
+
+module.exports = {
+	DISC_TOKEN,
+	DISC_GUILD_ID,
+	DISC_CLIENT_ID,
+};
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+export const DISC_TOKEN = '';
+export const DISC_GUILD_ID = '';
+export const DISC_CLIENT_ID = '';
+
+export const BB_KEY = '';
+export const BB_CLIENT_ID = '';
+export const BB_CLIENT_SECRET = '';
+export const BB_BASIC = '';
+
+export const PIX_CHAVE = 'testqrcode01@bb.com.br';
+
+// module.exports = {
+// 	DISC_TOKEN,
+// 	DISC_GUILD_ID,
+// 	DISC_CLIENT_ID,
+// 	BB_KEY,
+// 	BB_CLIENT_ID,
+// 	BB_CLIENT_SECRET,
+// };
